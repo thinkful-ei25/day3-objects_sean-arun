9b5706198799d6b35f3c211434e70920f6b2d358
Arun Seehra <seehra.a@gmail.com>
2018-09-26 11:54:15 -0500

    add validate keys

--- objectDrills2_arun_sean.js
+++ objectDrills2_arun_sean.js
@@ -150,7 +150,7 @@ function findById(items, idNum) {
 
 //
 
-function testIt() {
+(function testIt() {
   const testData = [
     { id: 1, foo: 'bar' },
     { id: 2, foo: 'bizz' },
@@ -174,7 +174,135 @@ function testIt() {
     return;
   }
   console.log('SUCCESS: `findByid` is working');
+})();
+
+// running the function with `objectA` and `expectedKeys`
+// should return `true`
+const objectA = {
+  id: 2,
+  name: 'Jane Doe',
+  age: 34,
+  city: 'Chicago',
+};
+
+// running the function with `objectB` and `expectedKeys`
+// should return `false`
+const objectB = {
+  id: 3,
+  age: 33,
+  city: 'Peoria',
+};
+
+const expectedKeys = ['id', 'name', 'age', 'city'];
+
+function validateKeys(object, expectedKeys) {
+  const objectKeys = Object.keys(object);
+
+  if (objectKeys.length !== expectedKeys.length) {
+    return false;
+  }
+
+ return (objectKeys.filter(key => expectedKeys.includes(key)).length === expectedKeys.length) ? true : false;
+
+  // const objectKeys = Object.keys(object);
+  // let numValidKeys = 0;
+
+  // expectedKeys.forEach(key => {
+  //   if (objectKeys.includes(key)) {
+  //     numValidKeys += 1;
+  //   }
+  // });
+
+  // if (objectKeys.length === numValidKeys && expectedKeys.length === numValidKeys) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 }
 
-testIt();
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
 
+(function testIt() {
+  const objectA = {
+    id: 2,
+    name: 'Jane Doe',
+    age: 34,
+    city: 'Chicago',
+  };
+
+  const objectB = {
+    id: 3,
+    age: 33,
+    city: 'Peoria',
+  };
+
+  const objectC = {
+    id: 9,
+    name: 'Billy Bear',
+    age: 62,
+    city: 'Milwaukee',
+    status: 'paused',
+  };
+
+  const objectD = {
+    foo: 2,
+    bar: 'Jane Doe',
+    bizz: 34,
+    bang: 'Chicago',
+  };
+
+  const expectedKeys = ['id', 'name', 'age', 'city'];
+
+  if (typeof validateKeys(objectA, expectedKeys) !== 'boolean') {
+    console.error('FAILURE: validateKeys should return a boolean value');
+    return;
+  }
+
+  if (!validateKeys(objectA, expectedKeys)) {
+    console.error(
+      `FAILURE: running validateKeys with the following object and keys
+      should return true but returned false:
+      Object: ${JSON.stringify(objectA)}
+      Expected keys: ${expectedKeys}`
+    );
+    return;
+  }
+
+  if (validateKeys(objectB, expectedKeys)) {
+    console.error(
+      `FAILURE: running validateKeys with the following object and keys
+      should return false but returned true:
+      Object: ${JSON.stringify(objectB)}
+      Expected keys: ${expectedKeys}`
+    );
+    return;
+  }
+
+  if (validateKeys(objectC, expectedKeys)) {
+    console.error(
+      `FAILURE: running validateKeys with the following object and keys
+      should return false but returned true:
+      Object: ${JSON.stringify(objectC)}
+      Expected keys: ${expectedKeys}`
+    );
+    return;
+  }
+
+  if (validateKeys(objectD, expectedKeys)) {
+    console.error(
+      `FAILURE: running validateKeys with the following object and keys
+      should return false but returned true:
+      Object: ${JSON.stringify(objectD)}
+      Expected keys: ${expectedKeys}`
+    );
+    return;
+  }
+
+  console.log('SUCCESS: validateKeys is working');
+})();
