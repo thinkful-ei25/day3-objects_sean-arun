2dc6f82320770f8e0ed5550a7f4839fd820a9fef
Sean Nealon <signalflowsean@gmail.com>
2018-09-26 14:25:11 -0400

    properties that aren\'t there

--- objectDrills3_arun_sean.js
+++ objectDrills3_arun_sean.js
@@ -32,12 +32,24 @@ const today = {
 
 console.log(today.meals[3]); 
 
-const hobbit = {name: 'Frodo', jobTitle: 'Keep of the Ring'};
+// const comedian = {name: 'Jim Carey', jobTitle: 'Pet Detective'};
+// const hobbit = {name: 'Frodo', jobTitle: 'Keep of the Ring', boss : 'gandalf'};
+// const demonHunter = {name: 'Wynonna Earp', jobTitle: 'Holder of Peacekeeper', boss : 'Dolls'};
+
+// const people = [hobbit, comedian, demonHunter];
+
+// people.forEach(person => {
+//   if (person.boss) console.log(`${person.jobTitle} ${person.name} reports to ${person.boss} `);
+//   else console.log(`${person.jobTitle} ${person.name} doesn't report to anybody`); 
+// });
+
 const comedian = {name: 'Jim Carey', jobTitle: 'Pet Detective'};
-const demonHunter = {name: 'Wynonna Earp', jobTitle: 'Holder of Peacekeeper'};
+const hobbit = {name: 'Frodo', jobTitle: 'Keep of the Ring', boss : comedian};
+const demonHunter = {name: 'Wynonna Earp', jobTitle: 'Holder of Peacekeeper', boss : hobbit};
 
 const people = [hobbit, comedian, demonHunter];
 
 people.forEach(person => {
-  console.log(`${person.name}: ${person.jobTitle}`);
+  if (person.boss) console.log(`${person.jobTitle} ${person.name} reports to ${person.boss.name} `);
+  else console.log(`${person.jobTitle} ${person.name} doesn't report to anybody`); 
 });
