da976d07f9c1ea0417bdb444cc703b9ca8f42f28
Sean Nealon <signalflowsean@gmail.com>
2018-09-26 14:01:22 -0400

    added loaf object + hydration function

---
+++ objectDrills3_arun_sean.js
@@ -0,0 +1,14 @@
+'use strict'; 
+
+const loaf = {
+  flour : 300,
+  water : 210, 
+  hydration : function(){
+    return this.flour * 100; 
+  }
+}; 
+
+
+console.log(loaf.flour); 
+console.log(loaf.water); 
+console.log(loaf.hydration()); 
